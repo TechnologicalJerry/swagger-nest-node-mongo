614e3d2110d570949397c18de03d952f92d205dd
technologicaljerry <cserajatverma@gmail.com>
2023-04-18 22:41:47 +0530

    Update todos.controller.ts

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('todos')
 export class TodosController {
   @Get()
-  getAllUsersList() {
+  getAllTodosList() {
     return [];
   }
 }

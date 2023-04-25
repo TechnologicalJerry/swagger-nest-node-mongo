d2359f1ecde3e3fb02587e6282b2c32567b9da14
technologicaljerry <cserajatverma@gmail.com>
2023-04-25 23:26:22 +0530

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService { }
+export class UsersService {
+    private users: any = [{ id: 0, name: 'Jerry' }];
+}

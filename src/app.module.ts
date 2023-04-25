bc5053fc03ba3cd1a1ec3ba49a150098d3e0ec54
technologicaljerry <cserajatverma@gmail.com>
2023-04-25 21:10:12 +0530

    update in users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, TodosModule],
+  imports: [TodosModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {
-  @Get()
-  getAllUsersList() {
-    return [];
-  }
-}
+export class UsersController {}

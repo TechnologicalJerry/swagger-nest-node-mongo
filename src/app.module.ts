9d91ab4022a41a5ca8e252a2da2a1ad655059787
technologicaljerry <cserajatverma@gmail.com>
2023-04-26 23:43:16 +0530

    add product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [TodosModule, UsersModule],
+  imports: [TodosModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })

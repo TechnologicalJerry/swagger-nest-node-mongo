90c15508f9e622164ac878bd5b36cccc4961f4c4
technologicaljerry <cserajatverma@gmail.com>
2023-05-02 07:13:50 +0530

    Update fileupload.controller.ts

--- src/fileupload/fileupload.controller.ts
+++ src/fileupload/fileupload.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('fileupload')
-export class FileuploadController {}
+export class FileuploadController {
+
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    uploadFile(@UploadedFile() file: Express.Multer.File) {
+        console.log(file);
+    }
+}

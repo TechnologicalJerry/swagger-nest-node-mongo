eb1d438e57b1280572c3e674e0b4fb2ff0e28d06
technologicaljerry <cserajatverma@gmail.com>
2023-05-02 07:10:50 +0530

    Update fileupload.module.ts

--- src/fileupload/fileupload.module.ts
+++ src/fileupload/fileupload.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { FileuploadController } from './fileupload.controller';
 import { FileuploadService } from './fileupload.service';

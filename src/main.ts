a0bc7df9e1b406ed88a5dc2ca05a59753e658b25
technologicaljerry <cserajatverma@gmail.com>
2023-06-03 23:07:56 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const PORT = 5000;
+const HOST = 'LOCALHOST';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  Logger.log(`SERVER RUNNING ON:${HOST} AT:${PORT}`)
 }
 bootstrap();

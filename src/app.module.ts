0d1cec44740d312a73427a0a74f723bd5fff9aa0
TechnologicalJerry <cserajatverma@gmail.com>
2025-08-20 14:47:03 +0530

    Refactor project structure and update dependencies
    Removed todos, users, products, and fileupload modules and their related files. Migrated ESLint configuration to eslint.config.mjs and updated .gitignore for better coverage. Upgraded NestJS and related dependencies to v11, updated README with improved instructions and resources, and made minor config file adjustments.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,10 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodosModule } from './todos/todos.module';
-import { UsersModule } from './users/users.module';
-import { ProductsModule } from './products/products.module';
-import { FileuploadModule } from './fileupload/fileupload.module';
-// import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [
-    TodosModule,
-    UsersModule,
-    ProductsModule,
-    FileuploadModule,
-
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/users/users.service.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UsersService {
-    private users: any = [{ id: 0, name: 'Jerry' }];
-}

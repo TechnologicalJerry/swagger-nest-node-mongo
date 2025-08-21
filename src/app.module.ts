b5db7179cda0306b77bc13940310e18ffb629cd7
TechnologicalJerry <cserajatverma@gmail.com>
2025-08-22 02:25:08 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     UsersModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/swagger-nest-node-mongo'),
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/swagger-nest-node-mongo'),
   ],
   controllers: [AppController],
   providers: [AppService],

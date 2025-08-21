f4a03e73e06aa383bb5ec5d3132a8f27ce89ad74
TechnologicalJerry <cserajatverma@gmail.com>
2025-08-22 02:25:46 +0530

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User, UserSchema } from './schemas/user.schema';
+import { EmailService } from '../auth/email.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ConfigModule,
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, EmailService],
   exports: [UsersService],
 })
 export class UsersModule {}

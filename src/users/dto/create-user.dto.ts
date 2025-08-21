b3ace23483cc23ab1b339b76db75e896da9087e7
TechnologicalJerry <cserajatverma@gmail.com>
2025-08-21 22:50:59 +0530

    Update create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty, MinLength, IsOptional, IsEnum, Matches, Validate } from 'class-validator';
+import { Match } from '../decorators/match.decorator';
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  OTHER = 'other'
+}
 
 export class CreateUserDto {
   @ApiProperty({
-    description: 'The name of the user',
-    example: 'John Doe'
+    description: 'The first name of the user',
+    example: 'John'
   })
   @IsString()
   @IsNotEmpty()
-  name: string;
+  firstName: string;
+
+  @ApiProperty({
+    description: 'The last name of the user',
+    example: 'Doe'
+  })
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty({
+    description: 'The username of the user',
+    example: 'johndoe123'
+  })
+  @IsString()
+  @IsNotEmpty()
+  userName: string;
 
   @ApiProperty({
     description: 'The email address of the user',
@@ -19,6 +42,32 @@ export class CreateUserDto {
   email: string;
 
   @ApiProperty({
+    description: 'The phone number of the user',
+    example: '+1234567890'
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\+?[\d\s\-\(\)]+$/, { message: 'Please provide a valid phone number' })
+  phone: string;
+
+  @ApiProperty({
+    description: 'The gender of the user',
+    enum: Gender,
+    example: Gender.MALE
+  })
+  @IsEnum(Gender)
+  @IsNotEmpty()
+  gender: Gender;
+
+  @ApiProperty({
+    description: 'The address of the user',
+    example: '123 Main St, City, State 12345'
+  })
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @ApiProperty({
     description: 'The password of the user',
     example: 'password123',
     minLength: 6
@@ -27,4 +76,13 @@ export class CreateUserDto {
   @IsNotEmpty()
   @MinLength(6)
   password: string;
+
+  @ApiProperty({
+    description: 'Password confirmation - must match password',
+    example: 'password123'
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Match('password', { message: 'Password and confirmPassword do not match' })
+  confirmPassword: string;
 }
